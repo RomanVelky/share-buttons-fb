aea1f3cf209b083854f36a3f809354888fa36dd9
RomanVelky <rom.velky@gmail.com>
2024-08-31 06:22:55 +0200

    feat: ngx share buttons added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShareButtons } from 'ngx-sharebuttons/buttons'; // Import the ShareButtons component
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ShareButtons, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,41 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { ShareButtons } from 'ngx-sharebuttons/buttons'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { customShareButton, provideShareButtonsOptions } from 'ngx-sharebuttons'
+import {
+  faFacebookMessenger,
+  faFacebookSquare,
+  faRedditAlien,
+  faTelegramPlane,
+  faWhatsapp,
+  faXTwitter,
+} from '@fortawesome/free-brands-svg-icons'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    FontAwesomeModule,
+    provideShareButtonsOptions(
+      customShareButton('facebook', {
+        icon: faFacebookSquare,
+      }),
+      customShareButton('x', {
+        icon: faXTwitter, // Assuming `faXTwitter` is the correct icon, replace if necessary
+      }),
+      customShareButton('reddit', {
+        icon: faRedditAlien,
+      }),
+      customShareButton('telegram', {
+        icon: faTelegramPlane,
+      }),
+      customShareButton('messenger', {
+        icon: faFacebookMessenger,
+      }),
+      customShareButton('whatsapp', {
+        icon: faWhatsapp,
+      })
+    ),
+  ],
 };
